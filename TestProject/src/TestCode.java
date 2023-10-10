c2dcc021eecf150d10c0f5a036ca7ca80c10d19d
UDAY368 <udayabdm368@gmail.com>
2023-10-10 11:02:37 +0530

    This is my first commit

---
+++ TestProject/src/TestCode.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] ar = {20,10,30,50,40};
+		
+		Arrays.sort(ar);
+		
+		for (int i : ar) {
+			System.out.println(i);
+		}
+		
+	}
+}
